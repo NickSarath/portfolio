976fabfbce0ae9d5219d0f9e941e49946b2e8d0e
Calum Patrick <hello@calum.co>
2018-05-24 23:43:12 -0400

    case study

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Main from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render((<BrowserRouter>
+ReactDOM.render((<BrowserRouter basename={process.env.PUBLIC_URL}>
                     <Main />
                   </BrowserRouter>
 )               ,document.getElementById('root'));
